2f295a943c09bc57b4174c63a134c1985588b158
soumya shrivastava <55390921+somyashri3@users.noreply.github.com>
2022-02-17 01:20:03 +0530

    Create StringConcetenate.java

---
+++ StringConcetenate.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class Main {
+   public static void main(String args[]) {
+      Scanner sc = new Scanner(System.in);
+      System.out.println("Enter a string value: ");
+      String str = sc.nextLine();
+      String str2 = sc.nextLine();
+      
+      System.out.println(str+" "+str2);
+   }
+}

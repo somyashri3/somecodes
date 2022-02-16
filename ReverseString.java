b1b80f26cefe2924c10b045b5d8742576bde01d4
soumya shrivastava <55390921+somyashri3@users.noreply.github.com>
2022-02-17 01:08:04 +0530

    Create ReverseString.java

---
+++ ReverseString.java
@@ -0,0 +1,11 @@
+public class Main
+{
+	public static void main(String[] args) {
+String s="soumya";  
+for (int i=s.length()-1;i>=0;i--){
+    char ch= s.charAt(i);
+ System.out.print(ch); 
+   
+}  
+}
+}

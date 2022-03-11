81290ff841f06b7e460effec97f2df02b9d13db7
soumya shrivastava <55390921+somyashri3@users.noreply.github.com>
2022-03-12 04:00:14 +0530

    Update sol2.cpp

--- sol2.cpp
+++ sol2.cpp
@@ -159,3 +159,139 @@ break;
     return 0;
 }
 }
+#include <bits/stdc++.h>
+#include <iostream>
+using namespace std;
+string one[] = { "", "One","Two","Three","Four",
+         "Five ","Six","Seven","Eight",
+                "Nine","Ten","Eleven","Twelve",
+                "Thirteen","Fourteen","Fifteen",
+            "Sixteen", "Seventeen","Eighteen",
+                "Nineteen" };
+ 
+string ten[] = { "", "", "Twenty ", "Thirty ", "Forty ",
+                 "Fifty ", "Sixty ", "Seventy ", "Eighty ",
+                 "Ninety " };
+ 
+string number(int n, string s)
+{
+    string str = "";
+    if (n > 19)
+        str += ten[n / 10] + one[n % 10];
+    else
+        str += one[n];
+ 
+    if (n)
+        str += s;
+ 
+    return str;
+}
+ 
+string in_words(long n)
+{
+    string ans;
+ 
+    ans += number((n / 10000000), "Crore ");
+ 
+    ans += number(((n / 100000) % 100), "Lakh ");
+ 
+    ans += number(((n / 1000) % 100), "Thousand ");
+ 
+    ans += number(((n / 100) % 10), "Hundred ");
+ 
+    if (n > 100 && n % 100)
+        ans += "And ";
+ 
+    ans += number((n % 100), "");
+   
+    if(ans=="")
+    ans = "Zero";
+ 
+    return ans;
+}
+int main(){
+int n;
+  cin>>n;
+  int digit[n];
+  string s[n];
+  string iemo[n];
+  string emo_list[4]={"Happy","Sad","Neutral","None"};
+  int emo_point[4]={10,5,2,1};
+  int total_score=0;
+  for(int i=0;i<n;i++){
+  	cin>>digit[i];
+    cin>>s[i];
+    
+    // cout<<digit[i]<<s[i];
+  }
+    for(int i=0;i<n;i++){
+  	cin>>iemo[i];
+  }
+  for(int i=0;i<n;i++){
+    if(iemo[i]==emo_list[0]){
+  	total_score=(total_score+(digit[i]  * emo_point[0]));
+//   	cout<<digit[i]<<" "<<emo_point[0]<<" "<<total_score<<" ";
+    }
+    else if(iemo[i]==emo_list[1]){
+  	total_score=total_score+(digit[i] * emo_point[1]);
+//   	cout<<digit[i]<<" "<<emo_point[0]<<" "<<total_score<<" ";
+
+    }
+    else if(iemo[i]==emo_list[2]){
+  	total_score=total_score+(digit[i] * emo_point[2]);
+//   	cout<<digit[i]<<" "<<emo_point[0]<<" "<<total_score<<" ";
+
+    }
+    else if(iemo[i]==emo_list[3]){
+  	total_score=total_score+(digit[i]*emo_point[3]);
+//   	cout<<digit[i]<<" "<<emo_point[0]<<" "<<total_score<<" ";
+
+    }
+    // else
+    // // cout<<"np"<<" ";
+   }
+  //vowel count
+    int v_count = 0;
+    int total_v_count=0;
+  for(int j=0;j<n;j++){
+    int x=s[j].length();
+    for(int i = 0; i<x; i++)  
+    {
+        if(s[j][i]=='a'|| s[j][i]=='e'||s[j][i]=='i'||s[j][i]=='o'||s[j][i]=='u'
+        ||s[j][i]=='A'||s[j][i]=='E'||s[j][i]=='I'||s[j][i]=='O' ||s[j][i]=='U')
+        {
+		    v_count++;
+        }
+      
+    }
+    total_v_count=total_v_count+(v_count*digit[j]);
+    // cout<<"count"<<total_v_count<<" "<<v_count<<" "<<digit[j]<<" ";
+    v_count=0;
+  }
+ int num=total_score/total_v_count;
+  
+  //check prime
+
+   bool flag = true;
+
+   for(int i = 2; i <= num / 2; i++) {
+      if(num % i == 0) {
+         flag = false;
+         break;
+      }
+   }
+   if (flag==true)
+      cout<<"Yes";
+   else
+      cout<<"No";
+
+  //print num
+cout<<" "<< in_words(num); 
+}
+
+//new one
+
+
+
+
+
